866867133f281da314053494ad0eff33269d467a
Wendi Li <wendicodes@gmail.com>
2018-02-14 13:27:16 +0100

    restaurants controller

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
 
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+require 'faker'
+
+10.times do
+  Restaurant.create(
+    name: Faker::Food.dish,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian", "mexican", "thai", "spanish"].sample
+  )
+end
